3bf4c12e0797823153583495a266af1b76b78f10
Filip Chovanec <filip@chovanec.com>
2023-12-15 20:15:05 +0100

    Implement shell args for passchk2

--- passchk2/main.c
+++ passchk2/main.c
@@ -1,24 +1,38 @@
+/*
+Please note that is code was written with mistakes on purpose, and should not be used as a good example.
+*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int code = 1839;
 char buff [32];
 
 int compute(char* text) {
   int acc = 1;
-  while (*text != 0) {
-    acc += *text;
-    ++text;
+  // Intentional for assembly
+  long length = strlen(text);
+  for (int i = 0; i < length; ++i) {
+    acc += text[i];
   }
   return acc;
 }
 
-int main() {
-  printf("Enter secret key: ");
-  gets(buff);
-  if (compute(buff) == code) {
+int main(int argc, char** argv) {
+  char* pass;
+  if (argc == 2) {
+    pass = argv[1];  
+  } else {
+    printf("Enter secret key: ");
+    gets(buff); 
+    pass = buff;
+  }
+  
+  if (compute(pass) == code) {
     printf("ACCESS GRANTED");
   } else {
     printf("ACCESS DENIED");
   }
+  
   return 0;
 }
